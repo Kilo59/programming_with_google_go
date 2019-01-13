8588678ea75b1fb45851dc14d915f49dfbd021c5
Gabriel <gabriel59kg@gmail.com>
2019-01-13 18:53:31 -0500

    take user input and apply to map object

--- 0.4_makejson/makejson.go
+++ 0.4_makejson/makejson.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func getUserInput(promptMessage string) (userInputStr string) {
+	var userInputValue string
+
+	fmt.Println(promptMessage)
+	_, err := fmt.Scanln(&userInputValue)
+	if err != nil {
+		fmt.Println("Could not parse input")
+		os.Exit(1)
+	}
+	return userInputValue
+}
 
 func main() {
-	fmt.Println("makejson")
+	addressMap := map[string]string{
+		"name":    getUserInput("Enter a name:"),
+		"address": getUserInput("Enter an address:")}
+	fmt.Println(addressMap)
 }
