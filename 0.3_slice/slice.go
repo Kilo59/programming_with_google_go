94cb41cc699ed380d35a09d32dc3f621295d4c9e
Gabriel <gabriel59kg@gmail.com>
2019-01-13 17:05:23 -0500

    Week 3 (#2)
    * Modules 1-2
    
    * Rename
    
    * rename
    
    * Add notes stub
    
    * slice.go stub
    
    * example
    
    * redo following instructions
    
    * rename alternate
    
    * add notes for strconv package
    
    * getUserInput works
    
    * ignore vs code settings
    
    * add notes from Week 2
    
    * Week 3 notes
    
    * Initial loop
    
    * map notes
    
    * Structs
    
    * finish slice.go
    
    * add assignment note
    
    * alternate note
    
    * ignore peer folders
    
    * cleanup

---
+++ 0.3_slice/slice.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func getUserInput(promptMessage string) (userInt int) {
+	var userInputValue string
+
+	fmt.Println(promptMessage)
+	fmt.Scanln(&userInputValue)
+
+	userInt, err := strconv.Atoi(userInputValue)
+	if err != nil {
+		if userInputValue == "X" {
+			fmt.Println("Exiting loop")
+			os.Exit(0)
+		}
+		fmt.Println("Could not parse Integer")
+		os.Exit(1)
+	}
+	return userInt
+}
+
+func main() {
+	// capacity of 8 to account for number of tests required in review criteria
+	// Underlying array will not need to expand unless more than 5 integers are entered
+	// Assignment calls for initializing with length of 3
+	var integerSlice = make([]int, 3, 8)
+
+	// alternatively could have set condition of loop to be userInput != "X"
+	for {
+		userInput := getUserInput("Enter an Integer (X to quit):")
+		integerSlice = append(integerSlice, userInput)
+		sort.Ints(integerSlice)
+		fmt.Println(integerSlice)
+	}
+}
