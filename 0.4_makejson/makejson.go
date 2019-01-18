a9e13f52053f6b822e2953bef467102c36140427
Gabriel <gabriel59kg@gmail.com>
2019-01-18 18:52:08 -0500

    JSON from map

--- 0.4_makejson/makejson.go
+++ 0.4_makejson/makejson.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 )
@@ -9,18 +10,32 @@ func getUserInput(promptMessage string) (userInputStr string) {
 	var userInputValue string
 
 	fmt.Println(promptMessage)
+	// Scan breaks on spaces
 	itemsScanned, err := fmt.Scanln(&userInputValue)
 	if err != nil {
-		fmt.Println("\tCould not parse input")
+		fmt.Println("\tCould not parse input!")
 		fmt.Printf("\tItems scanned %d\n", itemsScanned)
 		os.Exit(1)
 	}
 	return userInputValue
 }
 
+func mapToJSON(mapInput map[string]string) (jsonB []byte) {
+	jsonB, err := json.Marshal(mapInput)
+	if err != nil {
+		fmt.Println("\tCould not marshal mapInput object!")
+		os.Exit(1)
+	}
+	return jsonB
+}
+
 func main() {
 	addressMap := map[string]string{
 		"name":    getUserInput("Enter a name:"),
 		"address": getUserInput("Enter an address:")}
+	// remove debug statement
 	fmt.Println(addressMap)
+
+	addressMapJSON := mapToJSON(addressMap)
+	fmt.Println(string(addressMapJSON))
 }
