bbe36180cd9fcc38f4a3fab2824718b6d5e58903
Gabriel <gabriel59kg@gmail.com>
2019-01-18 21:35:39 -0500

    test writing file

--- 0.4_read/read.go
+++ 0.4_read/read.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func stopOnError(err error, errorMessage string) {
+	if err != nil {
+		fmt.Println(errorMessage)
+		os.Exit(1)
+	}
+}
 
 func main() {
 	fmt.Println("read")
+	f, err := os.Create("outfile.txt")
+	stopOnError(err, "Could not create file")
+
+	barr := []byte{1, 2, 3}
+	nb1, err := f.Write(barr)
+	stopOnError(err, "Could not write byte array")
+	nb2, err := f.WriteString("Hello World")
+	stopOnError(err, "Could not write string")
+	fmt.Println(nb1, nb2)
 }
