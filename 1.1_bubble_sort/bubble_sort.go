7499a28eb27cb64822ef1a1f210ec5a176b84096
Gabriel <gabriel59kg@gmail.com>
2019-01-20 06:21:16 -0500

    bubble sort stub

---
+++ 1.1_bubble_sort/bubble_sort.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// Swap ...
+// Swaps positions of 2 adjacent elements
+func Swap(index int, intSlice []int) {
+	fmt.Println("Swap position")
+}
+
+// BubbleSort ...
+func BubbleSort(intSlice []int) {
+	fmt.Println(intSlice)
+}
+
+func main() {
+	fmt.Println("bubble sort")
+}
