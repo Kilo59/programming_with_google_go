5c4a96cab6a5f4a946319c3df54e3d1ebeed2289
Gabriel <gabriel59kg@gmail.com>
2019-02-10 14:15:59 -0500

    animal stub

---
+++ 1.3_animals/animals.go
@@ -0,0 +1,23 @@
+package main
+
+// func getUserInput(promptMessage string) (userInt float64) {
+// 	var userInputValue string
+// 	fmt.Print(promptMessage)
+// 	fmt.Scanln(&userInputValue)
+// 	userInt, err := strconv.ParseFloat(userInputValue, 64)
+// 	if err != nil {
+// 		fmt.Println("Could not parse Integer")
+// 		os.Exit(1)
+// 	}
+// 	return userInt
+// }
+
+// func getUserInputLine(promptMessage string) (userInputStr string) {
+// 	// using bufio to avoid "problems" with fmt.Scan and spaces
+// 	scanner := bufio.NewScanner(os.Stdin)
+// 	fmt.Println(promptMessage)
+// 	scanner.Scan()
+// 	return scanner.Text()
+// }
+
+func main() {}
