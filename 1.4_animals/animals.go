0dcfb901f5f5a035a3ffe0c820ed3c3da1450e5e
Gabriel <gabriel59kg@gmail.com>
2019-02-17 20:24:47 -0500

    finish

--- 1.4_animals/animals.go
+++ 1.4_animals/animals.go
@@ -2,52 +2,113 @@ package main
 
 import "fmt"
 
-func userRequest() (animal string, attributeRequest string) {
-	var a string
-	var r string
+func userRequest() (query string, animalName string, infoRequest string) {
+	var q string
+	var an string
+	var ir string
 	fmt.Print(">")
-	fmt.Scan(&a, &r)
-	return a, r
+	fmt.Scan(&q, &an, &ir)
+	return q, an, ir
 }
 
-// Animal ...
-type Animal struct {
+// Animal interface type
+type Animal interface {
+	Eat()
+	Move()
+	Speak()
+}
+
+// Cow ...
+type Cow struct {
 	food       string
 	locomotion string
 	noise      string
 }
 
-// Eat ...
-func (a Animal) Eat() {
-	fmt.Println(a.food)
+// Bird ...
+type Bird struct {
+	food       string
+	locomotion string
+	noise      string
 }
 
-// Move ...
-func (a Animal) Move() {
-	fmt.Println(a.locomotion)
+// Snake ...
+type Snake struct {
+	food       string
+	locomotion string
+	noise      string
 }
 
-// Speak ...
-func (a Animal) Speak() {
-	fmt.Println(a.noise)
+// Eat Cow...
+func (c Cow) Eat() {
+	fmt.Println(c.food)
+}
+
+// Eat Bird...
+func (b Bird) Eat() {
+	fmt.Println(b.food)
+}
+
+// Eat Snake...
+func (s Snake) Eat() {
+	fmt.Println(s.food)
+}
+
+// Move Cow...
+func (c Cow) Move() {
+	fmt.Println(c.locomotion)
+}
+
+// Move Bird...
+func (b Bird) Move() {
+	fmt.Println(b.locomotion)
+}
+
+// Move Snake...
+func (s Snake) Move() {
+	fmt.Println(s.locomotion)
+}
+
+// Speak Cow...
+func (c Cow) Speak() {
+	fmt.Println(c.noise)
+}
+
+// Speak Bird...
+func (b Bird) Speak() {
+	fmt.Println(b.noise)
+}
+
+// Speak Snake...
+func (s Snake) Speak() {
+	fmt.Println(s.noise)
 }
 
 func main() {
+	fmt.Println("newanimal <name> <animal type>")
+	fmt.Println("query <name> <info>")
 	animals := map[string]Animal{
-		"cow":   Animal{"grass", "walk", "moo"},
-		"bird":  Animal{"worms", "fly", "peep"},
-		"snake": Animal{"mice", "slither", "hsss"},
+		"cow":   Cow{"grass", "walk", "moo"},
+		"bird":  Bird{"worms", "fly", "peep"},
+		"snake": Snake{"mice", "slither", "hsss"},
 	}
-
+	namedAnimals := map[string]Animal{}
 	for {
-		a, r := userRequest()
-		switch r {
-		case "eat":
-			animals[a].Eat()
-		case "move":
-			animals[a].Move()
-		case "speak":
-			animals[a].Speak()
+		q, an, ir := userRequest()
+		switch q {
+		case "newanimal":
+			namedAnimals[an] = animals[ir]
+			fmt.Println("Created it!")
+		case "query":
+			switch ir {
+			case "eat":
+				namedAnimals[an].Eat()
+			case "move":
+				namedAnimals[an].Move()
+			case "speak":
+				namedAnimals[an].Speak()
+			}
 		}
+
 	}
 }
